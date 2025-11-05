f7d15f31f88cef52d1d1f48e9b7c19c2b6571798
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-11-05 01:21:30 -0300

    change enum

--- pkg/emitter/data.go
+++ pkg/emitter/data.go
@@ -1,7 +1,6 @@
 package emitter
 
 import (
-	"fmt"
 	"github.com/tecmise/connector-lib/pkg/adapters/outbound/shared_kernel"
 )
 
@@ -17,36 +16,6 @@ type EmitableMetadata struct {
 }
 
 type DTOEmitted[T any] struct {
-	Data      T         `validate:"required"`
-	Operation Operation `validate:"required"`
-}
-
-type Operation string
-
-const (
-	OperationCreate Operation = "CREATE"
-	OperationUpdate Operation = "UPDATE"
-	OperationDelete Operation = "DELETE"
-)
-
-// IsValid verifica se a operação é válida.
-func (o Operation) IsValid() bool {
-	switch o {
-	case OperationCreate, OperationUpdate, OperationDelete:
-		return true
-	default:
-		return false
-	}
-}
-
-func ParseOperation(s string) (Operation, error) {
-	op := Operation(s)
-	if !op.IsValid() {
-		return "", fmt.Errorf("operação inválida: %s", s)
-	}
-	return op, nil
-}
-
-func (o Operation) String() string {
-	return string(o)
+	Data      T      `validate:"required"`
+	Operation string `validate:"required"`
 }
