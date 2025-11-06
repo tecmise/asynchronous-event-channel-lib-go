cf5d6f50e275fea940e429c04ca91d65d39aec47
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-11-05 23:31:32 -0300

    change type

--- pkg/consumer/queue.go
+++ pkg/consumer/queue.go
@@ -5,13 +5,13 @@ import (
 )
 
 type SNSNotification struct {
-	Type              string            `json:"Type"`
-	MessageID         string            `json:"MessageId"`
-	SequenceNumber    string            `json:"SequenceNumber"`
-	TopicARN          string            `json:"TopicArn"`
-	Subject           string            `json:"Subject"`
-	Message           string            `json:"Message"`
-	Timestamp         time.Time         `json:"Timestamp"`
-	UnsubscribeURL    string            `json:"UnsubscribeURL"`
-	MessageAttributes map[string]string `json:"MessageAttributes"`
+	Type              string                 `json:"Type"`
+	MessageID         string                 `json:"MessageId"`
+	SequenceNumber    string                 `json:"SequenceNumber"`
+	TopicARN          string                 `json:"TopicArn"`
+	Subject           string                 `json:"Subject"`
+	Message           string                 `json:"Message"`
+	Timestamp         time.Time              `json:"Timestamp"`
+	UnsubscribeURL    string                 `json:"UnsubscribeURL"`
+	MessageAttributes map[string]interface{} `json:"MessageAttributes"`
 }
