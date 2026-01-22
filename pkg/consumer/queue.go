2b257042479fafcda8ee937a7e13158a9f86e3d9
Joede Fadel <joede.fadel@tecmise.com>
2026-01-22 17:43:26 -0300

    Updates SNS notification structure.
    Updates the SNSNotification struct to include message attributes.
    This change allows consumers to access additional metadata
    associated with SNS messages.

--- pkg/consumer/queue.go
+++ pkg/consumer/queue.go
@@ -5,15 +5,13 @@ import (
 )
 
 type SNSNotification struct {
-	Type           string    `json:"Type"`
-	MessageID      string    `json:"MessageId"`
-	SequenceNumber string    `json:"SequenceNumber"`
-	TopicARN       string    `json:"TopicArn"`
-	Subject        string    `json:"Subject"`
-	Message        string    `json:"Message"`
-	Timestamp      time.Time `json:"Timestamp"`
-	UnsubscribeURL string    `json:"UnsubscribeURL"`
-}
-
-type MessageAttributeValue struct {
+	Type              string                 `json:"Type"`
+	MessageID         string                 `json:"MessageId"`
+	SequenceNumber    string                 `json:"SequenceNumber"`
+	TopicARN          string                 `json:"TopicArn"`
+	Subject           string                 `json:"Subject"`
+	Message           string                 `json:"Message"`
+	Timestamp         time.Time              `json:"Timestamp"`
+	UnsubscribeURL    string                 `json:"UnsubscribeURL"`
+	MessageAttributes map[string]interface{} `json:"messageAttributes"`
 }
